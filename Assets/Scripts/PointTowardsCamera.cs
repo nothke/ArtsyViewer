aa8b0d2d3cc183167f90bf3f8efc02c05fd71c0d
Ivan Notaros <nothke@gmail.com>
2017-07-13 21:03:28 +0200

    Point swapper will not follow every frame now

--- Assets/Scripts/PointTowardsCamera.cs
+++ Assets/Scripts/PointTowardsCamera.cs
@@ -4,8 +4,18 @@ using UnityEngine;
 
 public class PointTowardsCamera : MonoBehaviour
 {
+    public bool updateInUpdate;
+
     public Transform target;
+
+
     void Update()
+    {
+        if (updateInUpdate)
+            transform.rotation = Quaternion.LookRotation((target.position - transform.position).normalized, Vector3.up);
+    }
+
+    public void UpdateLook()
     {
         transform.rotation = Quaternion.LookRotation((target.position - transform.position).normalized, Vector3.up);
     }
