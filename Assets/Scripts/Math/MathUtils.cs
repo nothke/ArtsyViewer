fe5fe09f382c1763f43742652e7596dd0db74234
Ivan Notaros <nothke@gmail.com>
2017-07-13 16:26:33 +0200

    Moved IcoSphere into Math folder and added closest point to MathUtils

---
+++ Assets/Scripts/Math/MathUtils.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class MathUtils
+{
+
+    public static int GetClosestPointIndex(Vector3 from, Vector3[] points)
+    {
+        float closest = Mathf.Infinity;
+        int closestIndex = 0;
+
+        for (int i = 0; i < points.Length; i++)
+        {
+            float sqrDistance = (points[i] - from).sqrMagnitude;
+
+            if (sqrDistance < closest)
+            {
+                closestIndex = i;
+                closest = sqrDistance;
+            }
+        }
+
+        return closestIndex;
+    }
+}
