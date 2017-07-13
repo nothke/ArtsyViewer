13bd0fe2b6eece3d73d1b2feadc81cff736d0007
Ivan Notaros <nothke@gmail.com>
2017-07-13 15:43:38 +0200

    Added CameraPivot and it now previews icosphere points

---
+++ Assets/Scripts/CameraPivot.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraPivot : MonoBehaviour
+{
+    [Range(0, 4)]
+    public int icoSphereRecursions = 0;
+    public float radius = 5;
+
+    public float directionGizmoLength = 0.1f;
+
+    Vector3[] points;
+
+    private void OnValidate()
+    {
+        points = IcoSphere.GetIcoSpherePoints(radius, icoSphereRecursions);
+        Debug.Log("Number of points: " + points.Length);
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (points == null) return;
+
+        for (int i = 0; i < points.Length; i++)
+        {
+            Vector3 origin = transform.TransformPoint(points[i]);
+            Vector3 dir = transform.position - origin;
+
+            Gizmos.color = new Color(1f, 1f, 1f, 1f);
+            Gizmos.DrawSphere(origin, 0.05f);
+            Gizmos.color = new Color(0.4f, 1f, 1f, 0.4f);
+            Gizmos.DrawRay(origin, dir.normalized * directionGizmoLength);
+        }
+    }
+}
