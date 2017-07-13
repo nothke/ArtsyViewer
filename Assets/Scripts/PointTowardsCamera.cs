2e215bcc9fea8da58af99ddba19a15ea8a5593d5
Ivan Notaros <nothke@gmail.com>
2017-07-13 18:59:14 +0200

    Point towards camera for billboard

---
+++ Assets/Scripts/PointTowardsCamera.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointTowardsCamera : MonoBehaviour
+{
+    public Transform target;
+    void Update()
+    {
+        transform.rotation = Quaternion.LookRotation((target.position - transform.position).normalized, Vector3.up);
+    }
+}
